643813a533ee7b0b4cb11202d8e6f8375452b87b
Pragya Agrawal <pragya222@gmail.com>
2025-01-26 23:51:17 -0600

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +87,7 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+load_dotenv()
 DATABASES = {
 
     'default': {
